55606c7e893acd180dd5353c61180dfef1494229
Tobias Wetzel <tobi.wetzel@web.de>
2015-03-30 14:51:47 +0200

    add website

--- __openerp__.py
+++ __openerp__.py
@@ -11,7 +11,7 @@
     """,
 
     'author': "Tobias Wetzel",
-    'website': "http://www.yourcompany.com",
+    'website': "https://github.com/tobwetzel/",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
